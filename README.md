45d0feb8625d1c293239b8b854be5939f7c26986
aimlrl <mlresearch.axis@gmail.com>
2024-07-30 15:05:08 +0530

    Created everything to work in Linux as well as Windows

---
+++ README.md
@@ -0,0 +1,1 @@
+# BADCCE5103-auto-annotate
---
+++ lib_install.bat
@@ -0,0 +1,6 @@
+pip3 install numpy
+pip3 install pandas
+pip3 install opencv-python
+pip3 install tensorflow==2.13.0
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install supervision autodistill autodistill-grounding-dino autodistill-clip
